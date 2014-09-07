ef6e80c2fd9bebeefd9b581ba65a43d97f2ac660
arongdari <arongdari@gmail.com>
2014-09-07 17:10:43 +0900

    add supervised topic model (stm)

---
+++ slda_gibbs.py
@@ -0,0 +1,150 @@
+import numpy as np
+import time
+from scipy.special import gammaln
+from numpy.linalg import solve
+
+def sampling_from_dist(prob):
+    thr = prob.sum() * np.random.rand()
+    new_topic=0
+    tmp = prob[new_topic]
+    while tmp < thr:
+        new_topic += 1
+        tmp += prob[new_topic]
+    return new_topic
+
+"""
+supervised topic model with normal distribution
+based on stochastic EM (gibbs-EM)
+"""
+class sLDA:
+    def __init__(self, numTopic, numWord, alpha=0.1, beta=0.01):
+        self.K = numTopic
+        self.W = numWord
+
+        #hyper-parameters
+        self.alpha = alpha
+        self.beta = beta
+
+        self.eta = np.random.normal(scale=5,size=self.K)
+        self.sigma = 1
+
+    def random_init(self, docs, responses):
+        """
+        Random initialization of topics
+        """
+        print 'random init'
+        #summary statistics
+
+        self.WK = np.zeros([self.W,self.K]) + self.beta
+        self.sumK = np.zeros([self.K]) + self.beta * self.W
+        self.doc_topic_sum = np.zeros([len(docs), self.K]) + self.alpha
+
+        #topic assignments
+        self.doc_topics = list()
+
+        for di in xrange(len(docs)):
+            doc = docs[di]
+            topics = np.random.randint(self.K, size = len(doc))
+            self.doc_topics.append(topics)
+
+            for wi in xrange(len(doc)):
+                topic = topics[wi]
+                word = doc[wi]
+                self.WK[word,topic] += 1
+                self.sumK[topic] +=1
+                self.doc_topic_sum[di, topic] += 1
+
+        print 'done'
+
+    def stochasticEM(self, max_iter, docs, responses):
+        """
+        gibbs sampling
+        """
+        prev = time.clock()
+
+        for iteration in xrange(max_iter):
+            prev = time.clock()
+
+            for di in xrange(len(docs)):
+                doc = docs[di]
+                for wi in xrange(len(doc)):
+                    word = doc[wi]
+                    old_topic = self.doc_topics[di][wi]
+
+                    self.WK[word,old_topic] -= 1
+                    self.sumK[old_topic] -= 1
+                    self.doc_topic_sum[di,old_topic] -= 1
+
+                    z_bar = np.zeros([self.K,self.K]) + self.doc_topic_sum[di,:] + np.identity(self.K)
+                    z_bar /= z_bar.sum(0)
+
+                    #update
+                    prob = (self.WK[word, :])/(self.sumK[:]) * (self.doc_topic_sum[di,:]) * np.exp(np.negative((responses[di] - np.dot(z_bar.T,self.eta))**2))
+
+                    new_topic = sampling_from_dist(prob)
+
+                    self.doc_topics[di][wi] = new_topic
+                    self.WK[word,new_topic] += 1
+                    self.sumK[new_topic] += 1
+                    self.doc_topic_sum[di,new_topic] += 1
+
+            #estimate parameters
+            z_bar = self.doc_topic_sum / self.doc_topic_sum.sum(1)[:,np.newaxis] # DxK
+            self.eta = solve(np.dot(z_bar.T,z_bar), np.dot(z_bar.T, responses) )
+
+            #compute mean absolute error
+            mae = np.abs(responses - np.dot(z_bar, self.eta)).sum()
+            print 'iteration',iteration, time.clock()-prev, mae, self.loglikelihood(docs)
+
+    def heldoutSampling(self, max_iter, heldout):
+        h_doc_topics = list()
+        h_doc_topic_sum = np.zeros([len(heldout), self.K]) + self.alpha
+
+        for di in xrange(len(heldout)):
+            doc = heldout[di]
+            topics = np.random.randint(self.K, size = len(doc))
+            h_doc_topics.append(topics)
+
+            for wi in xrange(len(doc)):
+                topic = topics[wi]
+                word = doc[wi]
+                h_doc_topic_sum[di, topic] += 1
+
+        for iteration in xrange(max_iter):
+            for di in xrange(len(heldout)):
+                doc = heldout[di]
+                for wi in xrange(len(doc)):
+                    word = doc[wi]
+                    old_topic = h_doc_topics[di][wi]
+
+                    h_doc_topic_sum[di,old_topic] -= 1
+
+                    #update
+                    prob = (self.WK[word, :])/(self.sumK[:]) * (self.doc_topic_sum[di,:])
+
+                    new_topic = sampling_from_dist(prob)
+
+                    h_doc_topics[di][wi] = new_topic
+                    h_doc_topic_sum[di,new_topic] += 1
+
+        return h_doc_topic_sum
+
+    def loglikelihood(self, docs):
+        """
+        likelihood function
+        does not contain normal distribution part
+        """
+        ll = 0
+
+        ll += len(docs) * gammaln(self.alpha*self.K)
+        ll -= len(docs) * self.K * gammaln(self.alpha)
+        ll += self.K * gammaln(self.beta*self.W)
+        ll -= self.K * self.W * gammaln(self.beta) 
+
+        for di in xrange(len(docs)):
+            ll += gammaln(self.doc_topic_sum[di,:]).sum() - gammaln(self.doc_topic_sum[di,:].sum())
+        for ki in xrange(self.K):
+            ll += gammaln(self.WK[:,ki]).sum() - gammaln(self.WK[:,ki].sum())
+
+        return ll
+
---
+++ slda_vb.py
@@ -0,0 +1,107 @@
+import numpy as np
+from scipy.special import gammaln, psi
+from numpy.linalg import solve
+
+eps = 1e-100
+
+class sLDA:
+
+    def __init__(self, docs, responses, vocab, num_topic, alpha = 0.1, sigma=1):
+        self.docs = docs
+        self.responses = np.array(responses)
+        self.vocab = vocab
+
+        self.D = len(docs)
+        self.W = len(vocab)
+        self.K = num_topic
+
+        #topic proportion for document
+        self.gamma = np.random.gamma(100., 1./100, (self.D, self.K))
+        #word-topic distribution
+        self.beta = np.random.gamma(100., 1./100, (self.K, self.W))
+        self.beta /= np.sum(self.beta,1)[:,np.newaxis]
+        #coefficient & covariance
+        self.eta = np.zeros(self.K)
+        self.sigma = sigma
+        self.alpha = alpha
+        self.dir_prior = 0.01
+        self.ssword = np.random.gamma(100., 1./100, (self.K, self.W))
+        self.EA = np.zeros((self.D,self.K))
+        self.EAA = np.zeros((self.K,self.K))
+        self.small_iter = 10
+
+    def do_e_step(self):
+        self.EA = np.zeros((self.D,self.K))
+        self.EAA = np.zeros((self.K,self.K))
+
+        e_log_beta = np.log(self.beta)
+
+        for di in xrange(self.D):
+            doc = self.docs[di]
+            doc_len = len(doc)
+            y = self.responses[di]
+
+            phi = self.gamma[di,:][:,np.newaxis] * self.beta[:, doc]        # K * N
+            phi /= np.sum(phi, 1)[:,np.newaxis]
+
+            e_log_theta = psi(self.gamma[di,:]) - psi(np.sum(self.gamma[di,:]))
+            #exact update phi
+            # for i in xrange(self.small_iter):
+            # for wi in xrange(len(doc)):
+            #     word = doc[wi]
+            #     phi_sum = np.sum(phi, 1) - phi[:,wi]    # K dim
+            #     rss = (2. * np.dot(phi_sum, self.eta) * self.eta + (self.eta ** 2))/(2.* (np.float(doc_len)**2.) * (self.sigma**2.) )
+            #     phi[:,wi] = np.exp(e_log_theta + e_log_beta[:,word] + (y/(np.float(doc_len)*(self.sigma**2.)))*self.eta - rss )
+            #     phi[:,wi] /= phi[:,wi].sum()
+
+            #approximately update phi
+            phi_sum = np.sum(phi, 1)[:,np.newaxis] - phi
+            rss = (2. * np.dot(self.eta, phi_sum)[:,np.newaxis] * self.eta + (self.eta ** 2))/(2.* (np.float(doc_len)**2.) * (self.sigma**2.) )
+            rss = rss.T     # K x len
+            phi = np.exp(e_log_theta[:,np.newaxis] + e_log_beta[:,doc] + ((y/(np.float(doc_len)*(self.sigma**2.)))*self.eta)[:,np.newaxis] - rss + eps)
+            phi /= np.sum(phi, 0)
+
+            #update gamma
+            self.gamma[di, :] = np.sum(phi, 1) + self.alpha
+
+            #ssword for updating bata
+            for wi in xrange(len(doc)):
+                word = doc[wi]
+                self.ssword[:,word] += phi[:,wi]
+
+            phi_norm = np.sum(phi, 1)
+            phi_norm /= phi_norm.sum()
+
+            self.EA[di,:] = phi_norm 
+            self.EAA += np.outer(phi_norm, phi_norm)
+
+    def do_m_step(self):
+        #update beta
+        self.beta = self.ssword / np.sum(self.ssword, 1)[:,np.newaxis]
+        self.ssword = np.zeros(self.beta.shape) + self.dir_prior
+
+        self.eta = solve(self.EAA, np.dot(self.EA.T, self.responses))
+
+        #compute mean absolute error
+        mae = np.abs(np.dot(self.EA, self.eta) - self.responses).sum()
+        return mae
+
+    def heldoutEstep(self, max_iter, heldout):
+        gamma = np.random.gamma(100., 1./100, (len(heldout), self.K))
+
+        for iter in xrange(max_iter):
+            for di in xrange(len(heldout)):        
+                doc = heldout[di]
+                doc_len = len(doc)
+
+                phi = np.zeros([self.K, doc_len])
+
+                e_log_theta = psi(self.gamma[di,:]) - psi(np.sum(self.gamma[di,:]))
+                phi = self.beta[:,doc] * np.exp(e_log_theta)[:,np.newaxis]
+                phi /= np.sum(phi, 0)
+
+                #update gamma
+                gamma[di, :] = np.sum(phi, 1) + self.alpha
+
+        return gamma
+
